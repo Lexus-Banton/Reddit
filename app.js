c5f4eb1079a10b0896a0c2c60a3ac252b57247e8
Lexus Banton <lexusbanton.kr@gmail.com>
2022-10-06 16:14:02 -0700

    View all posts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { getPosts } from './fetch-utils.js';
 import { renderPost } from './render-utils.js';
 
 /* Get DOM Elements */
@@ -11,6 +12,18 @@ let error = null;
 let posts = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await getPosts();
+    error = response.error;
+    posts = response.data;
+
+    if (error) {
+        displayError();
+    }
+    if (posts) {
+        displayPosts();
+    }
+});
 
 /* Display Functions */
 function displayPosts() {
--- render-utils.js
+++ render-utils.js
@@ -1,8 +1,8 @@
 export function renderPost(post) {
     const li = document.createElement('li');
 
-    const a = document.createElement('a');
-    a.href = `/post/?id=${post.id}`;
+    //const a = document.createElement('a');
+    //a.href = `/reddit/?id=${post.id}`;
 
     const h2 = document.createElement('h2');
     h2.textContent = post.title;
@@ -13,8 +13,8 @@ export function renderPost(post) {
     const img = document.createElement('img');
     img.src = post.image_url;
 
-    a.append(h2, p);
-    li.append(a);
+    li.append(h2, p, img);
+    //li.append();
 
     return li;
 }
